0e8ad9af417725f4d010b9297181b75712473c81
Pau Gual Ahmad <p0xer404@gmail.com>
2025-09-15 00:51:27 +0200

    Reparado error para acceder del vite.config.js al solo estar definido para host

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // exposa la IP de la Raspberry
+    port: 5173,  // pots deixar el port per defecte
+  },
 });
